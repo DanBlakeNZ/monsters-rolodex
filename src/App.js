560bb370bb33c7941ffadb0eba9d06dadcc6d363
DanBlakeNZ <dblakenz@hotmail.com>
2020-07-14 10:49:04 +1000

    add search, final styles, gh-pages

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -26,7 +30,11 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <SearchBox placeholder="search monsters" handleChange={(e) => this.setState({ searchField: e.target.value })} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
