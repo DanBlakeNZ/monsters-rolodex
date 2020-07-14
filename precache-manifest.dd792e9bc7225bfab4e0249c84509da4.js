32e762f996d78042a965f8cde50238a743a61a2b
DanBlakeNZ <dblakenz@hotmail.com>
2020-07-14 10:49:53 +1000

    Updates

---
+++ precache-manifest.dd792e9bc7225bfab4e0249c84509da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf75cf70905bbb1cd298ea2758c49db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3bb51a23f524c65c542c",
+    "url": "/monsters-rolodex/static/css/main.f5bb50ab.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb51a23f524c65c542c",
+    "url": "/monsters-rolodex/static/js/main.65cdc9ac.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
